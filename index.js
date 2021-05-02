63945a08fbeb189e053f26e8a7b60aaea8069c9e
Isaaz Garcia <isaazgarciawork@gmail.com>
2021-05-01 21:01:01 -0700

    Adding some logs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 require('dotenv').config()
 const asana = require('asana')
+const chalk = require('chalk')
 const fs = require("fs")
 const yargs = require("yargs")
 
@@ -28,6 +29,17 @@ const fileToUpload = argv.file || process.env.FILE_TO_UPLOAD
 const taskId = argv.taskId || process.env.ASANA_TASK_ID
 const accessToken = argv.accessToken || process.env.ASANA_ACCESS_TOKEN
 
+if (!fileToUpload || !taskId || !accessToken) {
+    console.log(chalk.red(`
+${!fileToUpload? 'Set a file to upload with -f. Example: -f=file.txt' : ''}
+${!taskId? 'Set an Asana Task Id with -tid. Example: -tid=123456' : ''}
+${!accessToken? 'Set an Asana Personal Access Token with -at. Example: -at=123456' : ''}
+    `))
+    return
+}
+
+console.log(chalk.yellow("\n⚡︎" + (`Uploading ${fileToUpload} to Asana!`))+"\n")
+
 const client = asana.Client.create().useAccessToken(accessToken)
 const params = {
     method: 'POST',
@@ -40,6 +52,8 @@ const params = {
     },
 }
 
-client.dispatcher.dispatch(params, {})
-
-
+client.dispatcher.dispatch(params, {}).then(function (){
+    console.log(chalk.yellow("\n⚡Done uploading to Asana!" +"\n"))
+}).catch(function (){
+    console.log(chalk.red(`Done uploading to Asana!`)+"\n")
+})
